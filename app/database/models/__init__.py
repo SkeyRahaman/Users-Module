f614edd10fcd5b6a52f334ef4793d3bff6ae8bf2
SkeyRahaman <sakibmondal7@gmail.com>
2025-08-17 22:10:05 +0530

    Removed the code for manual tables creation as we are using alembic now.

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -5,10 +5,6 @@ from sqlalchemy.orm import declarative_base
 engine = create_async_engine(Config.DATABASE_URL)
 SessionLocal = async_sessionmaker(autoflush=False, autocommit = False, bind=engine)
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
 Base = declarative_base()
 
 #tables
--- app/main.py
+++ app/main.py
@@ -1,23 +1,11 @@
 from fastapi import FastAPI
-from app.database.models import Base, engine
 from app.config import Config
 from app.api.routers import users, auth, permissions, groups, roles, health
-from app.database.models import create_db_and_tables
-from contextlib import asynccontextmanager
-
-
-@asynccontextmanager
-async def lifespan(app : FastAPI):
-    # Code before yield runs at startup
-    await create_db_and_tables()
-    yield
-    # Code after yield runs at shutdown
 
 app = FastAPI(
     title="Users Module",
     description="Endpoints related to users authentications and authorizations.",
-    version=Config.VERSION,
-    lifespan=lifespan
+    version=Config.VERSION
 )
 
 app.include_router(health.router)
